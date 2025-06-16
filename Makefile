012c69bf53913fa6ef2e8e7e5052175cf07f7a2f
marcomit <marcomenegazzi.05@gmail.com>
2025-06-16 08:24:17 +0200

    target directory

--- Makefile
+++ Makefile
@@ -2,34 +2,45 @@
 CC = gcc
 CFLAGS = -Wall -Wextra -g
 
-# Executable name
-TARGET = pdp8
+# Folders
+BUILD_DIR = target
+
+# Executable name and path
+TARGET = $(BUILD_DIR)/pdp8
 
 # Source files
 SRCS = main.c pdp8.c tokenizer.c
 
-# Object files (automatically generated from SRCS)
-OBJS = $(SRCS:.c=.o)
+# Object files in BUILD_DIR
+OBJS = $(SRCS:%.c=$(BUILD_DIR)/%.o)
 
-# Default target (build everything)
+# Default target
 all: $(TARGET)
 
 # Link object files into executable
 $(TARGET): $(OBJS)
 	$(CC) $(CFLAGS) -o $@ $^
 
-# Compile each .c file into .o file
-%.o: %.c dlist.h
-	$(CC) $(CFLAGS) -c $<
+# Compile .c files to .o files in target/
+$(BUILD_DIR)/%.o: %.c | $(BUILD_DIR)
+	$(CC) $(CFLAGS) -c $< -o $@
 
-# Clean up build files
+# Ensure target directory exists
+$(BUILD_DIR):
+	mkdir -p $(BUILD_DIR)
+
+# Clean build
 clean:
-	rm -f $(OBJS) $(TARGET)
+	rm -rf $(BUILD_DIR)
 
-run:
-	make && ./$(TARGET)
+# Build and run
+run: all
+	echo ./$(TARGET)
 
+# Clean then run
 run-clean:
-	run clean
-# Mark targets that don't produce files
-.PHONY: all clean
+	make clean && make run
+
+# Mark targets as not files
+.PHONY: all clean run run-clean
+
--- tokenizer.c
+++ tokenizer.c
@@ -4,55 +4,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static bool is_important(char c) { return isalnum(c) || c == ',' || c == ' '; }
+static bool is_important(char c) { return isalnum(c) || c == '-'; }
 
-static void handle_line(char *line) {
-  int j = 0;
-  char formatted[1024];
-  for (int i = 0; line[i]; i++) {
-    if (line[i] == '/') {
-      break;
-    }
-    if (line[i] == ' ' && j == 0)
-      continue;
-    if (!is_important(line[i])) {
-      continue;
-    }
-    line[j++] = line[i];
-  }
-  line[j] = '\0';
+static void parse_token(Lexer *lexer, char *word, int row, int col) {
+
+  // ciao
+  printf("(%d,%d)%s\n", row, col, word);
 }
-
-static void parse_token(Lexer *lexer, char *word) { printf("%s\n", word); }
 
 /*
  * This function must be parse a row and it can generate an expression
  */
-static void parse_row(Lexer *lexer, char *line) {
+static void parse_row(Lexer *lexer, char *line, int row) {
   char c[1024];
   int j = 0;
   for (int i = 0; line[i]; i++) {
-    if (!isalnum(line[i])) {
+    if (line[i] == '/')
+      goto end;
+    if (line[i] == ',') {
+      parse_token(lexer, c, row, i);
+      j = 0;
+      c[j] = '\0';
+      continue;
+    }
+    if (!is_important(line[i])) {
       if (j > 0) {
         c[j] = '\0';
-        parse_token(lexer, c);
+        parse_token(lexer, c, row, i);
         j = 0;
       }
       continue;
     }
     c[j++] = line[i];
-    // if (line[i] == '/')
-    //   return;
-    // if (!isalnum(line[i])) {
-    //   if (j > 0) {
-    //     c[j] = '\0';
-    //     parse_token(lexer, c);
-    //   }
-    //   continue;
-    // }
-    // c[j++] = line[i];
   }
-  printf("\nEnd of the line\n\n");
+end:
+  printf("----\n");
 }
 
 static void read_file(const char *filename) {
@@ -69,11 +55,12 @@ Lexer *tokenize(const char *filename) {
   FILE *fd = fopen(filename, "r");
   Lexer *lexer = malloc(sizeof(Lexer));
   char line[1024];
+  int row = 0;
   while (fgets(line, 1024, fd)) {
-    handle_line(line);
+    // handle_line(line);
     if (line[0] == '\0')
       continue;
-    parse_row(lexer, line);
+    parse_row(lexer, line, row++);
   }
 
   return lexer;
